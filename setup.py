606e74fcc5394d5ef2a70977bb9874ea42dffc1d
Rodrigo Aluizio <r.aluizio@gmail.com>
2018-09-29 07:08:56 -0300

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PCRCalc',
+    version='1.0.1',
+    url='https://github.com/Godrigos/PCRCalc',
+    license='GPL-3.0',
+    author='Rodrigo Aluizio',
+    author_email='',
+    description='A PCR Mix Calculator',
+    long_description=long_description,
+    package_data={
+        '': ['images/*.png', 'presets/*.json', 'pcrcalc.ico'],
+    },
+    install_requires=['json', 'pillow', 'reportlab', 'ttkthemes'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+)
