56a2f6845734bfbc4a3e23d8cc012f86cbbb06cf
Rodrigo Aluizio <r.aluizio@gmail.com>
2018-10-01 17:15:47 -0300

    Additional files relative path added.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PCRCalc',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/Godrigos/PCRCalc',
     license='GPL-3.0',
     author='Rodrigo Aluizio',
