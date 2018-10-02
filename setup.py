d245b2e24e8bc91bf2811f6ac989ca39dafce851
rodrigo <r.aluizio@gmail.com>
2018-10-02 11:32:50 -0300

    setup.py versioning update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PCRCalc',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/Godrigos/PCRCalc',
     license='GPL-3.0',
     author='Rodrigo Aluizio',
